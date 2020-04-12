44b4765631429da46feabf15febed565b8c48071
Nico550363 <y5ut010@gmail.com>
2020-04-13 02:43:53 +0900

    toppage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'products#index'
   get "products" => "products#index"
+  get "products/skill" => "products#skill"
   get "products/about" => "products#about"
+  get "products/works" => "products#works"
+  get "products/contact" => "products#contact"
 end
